d2da52e2358b270d7da34f991b397f8cb11391a8
Ilya V. Schurov <ilya@schurov.com>
2013-05-01 16:15:23 +0400

    attrlib restored in master

---
+++ attrlib.h
@@ -0,0 +1,43 @@
+#include <assert.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
+#ifndef ATTRLIB_H
+#define ATTRLIB_H
+inline double f(int i, double x, double la, double f1fixedpoint)
+{
+    if(i==0 or i==2 or i==3)
+    {
+	return x-0.1*sin(2.3*M_PI*x-0.2*M_PI);
+    }
+    else if(i==1)
+    {
+	return (x-f1fixedpoint)*(1-la)+f1fixedpoint; 
+    }
+    assert(false);
+}
+inline int digit(double x)
+{
+    return int(x*2);
+}
+inline double drand(double x)
+{
+	return rand()/(((double)RAND_MAX + 1) / x);
+}
+inline double mod1(double x)
+{
+	if(x>=0 && x<1)
+	{
+		return x;
+	}
+	if(x>=1)
+	{
+		return x-int(x);
+	}
+	if(x<1)
+	{
+		return x-int(x)+1;
+	}
+}
+#endif
